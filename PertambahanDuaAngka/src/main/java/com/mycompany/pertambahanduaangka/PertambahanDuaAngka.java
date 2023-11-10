59dc8d9ac97cf8b2440a17983b38fff6bbf88132
AnggiOkaQamara <149868348+AnggiOkaQamara@users.noreply.github.com>
2023-11-10 22:10:04 +0800

    Add files via upload

---
+++ PertambahanDuaAngka/src/main/java/com/mycompany/pertambahanduaangka/PertambahanDuaAngka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertambahanduaangka;
+
+/**
+ *
+ * @author user
+ */
+public class PertambahanDuaAngka {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
